31cc593249a97a164d7a145b4d4bb0684f6608ad
mazharvai007 <swopnil420@gmail.com>
2025-05-07 01:18:20 +0600

    make group by item with summary

--- projects/5.shopping-cart-checkout-system/shopping-cart-checkout-system.py
+++ projects/5.shopping-cart-checkout-system/shopping-cart-checkout-system.py
@@ -1,7 +1,12 @@
-number_of_products = int(input("Number of products: "))
+number_of_products = int(input("Number of Products: "))
 
 
+# View the shopping cart
 def view_cart():
+    """
+    We will show the products in the cart with quantity, price, total price, discount etc.
+    """
+
     total_product_price = product_quantity * product_price
     discount_price = 0
 
@@ -10,25 +15,14 @@ def view_cart():
     if total_product_price > 1000:
         discount_price = promotional_discount * total_product_price
 
-    return f"\nProduct Name: {product_name}\nQuantity: {product_quantity}\nTotal Product Price: {total_product_price}\nDiscount (10% Off if total price is greater then 1000) Price: {discount_price}\nGrand Total: {total_product_price - discount_price}\n"
-
-
-def check_value(get_value: int, topic_name: str):
-    """
-    We are checking integer value
-    """
-
-    if not (get_value > 0):
-        print(
-            f"You entered invalid value {get_value} for {topic_name}. You must enter integer value"
-        )
-        return int(input(f"Enter value for {topic_name}: "))
-    return get_value
+    print(
+        f"\nProduct Name: {product_name}\nQuantity: {product_quantity}\nTotal Product Price: {total_product_price}\nDiscount (10% Off if total price is greater then 1000) Price: {discount_price}\nGrand Total: {total_product_price - discount_price}\n"
+    )
 
 
 for num in range(1, number_of_products + 1, 1):
-    product_name = input(f"Enter product {num} name: ")
-    product_quantity = check_value(int(input("Quantity of the product: ")), "Quantity")
-    product_price = check_value(int(input("Price of the product: ")), "Price")
+    product_name = input(f"Enter Product{num} Name: ")
+    product_quantity = int(input("Quantity of the Product: "))
+    product_price = int(input("Price of the Product: "))
 
-    print(view_cart())
+    view_cart()
--- projects/6.daily-sales-report-generator/daily-sales-report-generator.py
+++ projects/6.daily-sales-report-generator/daily-sales-report-generator.py
@@ -1,3 +1,18 @@
-product_name = input("Enter Product Name: ")
-product_quantity = int(input("Quantity of the product: "))
-per_unit_price = int(input("Price per unit: "))
+# Enter number of products
+number_of_products = int(input("Enter number of sales item: "))
+
+
+def sales_summery():
+    total_unit_price = product_quantity * per_unit_price
+    print(
+        f"\nProduct Name: {product_name}\nQuantity: {product_quantity}\nPer Unit Price: {per_unit_price}\nTotal Revenue: {total_unit_price}\n"
+    )
+
+
+# Control number of products
+for num in range(1, number_of_products + 1, 1):
+    product_name = input(f"Enter Product{num} Name: ")
+    product_quantity = int(input("Quantity of the product: "))
+    per_unit_price = int(input("Price per unit: "))
+
+    sales_summery()
